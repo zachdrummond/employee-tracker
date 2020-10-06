9b3bb2d9d18ab41dc2fd674eec26a2024fea0a5a
Zach Drummond <zachdrummond3@gmail.com>
2020-10-05 20:04:15 -0400

    Added viewAllEmployees function

--- index.js
+++ index.js
@@ -2,22 +2,23 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "mysql",
-    database: "employee_tracker_db"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "mysql",
+  database: "employee_tracker_db",
 });
 
-connection.connect(err => {
-    if (err) throw err;
-    name();
+connection.connect((err) => {
+  if (err) throw err;
+  viewAllEmployees();
 });
 
-viewAllEmployees = () => {
-    const sql = "SELECT * FROM employee";
-    connection.query(sql, (err, res) => {
-        console.table(res);
-        connection.end();
-    });
-}
+// Add Manager
+TODO: viewAllEmployees = () => {
+  const sql = ``;
+  connection.query(sql, (err, res) => {
+    console.table(res);
+    connection.end();
+  });
+};
